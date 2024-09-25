f861fb1013d164141cf302b07d9c008fa2f11f8a
Adrian Abella <fasttaco1@gmail.com>
2024-09-25 18:21:18 -0400

    Header of flashcard information added + Card outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
+import Card from "./components/Card.jsx";
 import "./App.css";
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+    return (
+        <div className="App">
+            <div className="header">
+                <h1>American Political History Quiz</h1>
+                <h3>How well do you know the political history of the US?</h3>
+                <p>Number of Cards: 10</p>
+            </div>
+            <div className="card-container">
+                <Card />
+                <button>⟶</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
